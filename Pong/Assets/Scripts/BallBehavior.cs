a369e6734309033d49724992d1df4435ba81c2e8
WillDavid <wdma.lic17@uea.edu.br>
2020-09-01 22:10:40 -0300

    Add Project NeonPong
    Proximo passo: Add Neon Material

---
+++ Pong/Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    public float speed = 5;
+
+
+    void Start()
+    {
+
+        //Sortear numeros entre 0 e 2
+        float speedX = Random.Range(0,2);
+        if(speedX == 0){
+            speedX = -1;
+        }else{
+            speedX = 1;
+        }
+        float speedY = Random.Range(0,2);
+        if(speedY == 0){
+            speedY = -1;
+        }else{
+            speedY = 1;
+        }
+
+
+        GetComponent<Rigidbody>().velocity = new Vector3(speed * speedX, speed * speedY, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
